f5af17dddc2c9432af1a5bf213963280c61c4b94
José Atay <108359607+joseatay11@users.noreply.github.com>
2022-11-21 18:08:28 -0500

    Add files via upload

---
+++ Ejercicio-2/arrays.js
@@ -0,0 +1,13 @@
+const lista = {
+    nombre: "José",
+    edad: 34,
+    soyDesarrollador: true,
+    fechaNacimiento: new Date(1988, 9, 11),
+    libroFavorito: {
+        titulo: "Todo el Mundo Debería Saber Programar: La Programación Web",
+        autor: "Marc Canido",
+        fecha: new Date(2019, 0, 1),
+        url: "https://www.buscalibre.com.co/libro-todo-el-mundo-deberia-saber-programar-la-programacion-web/9781709695148/p/53442066"
+    },
+};
+
