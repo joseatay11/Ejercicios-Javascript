7a830f64de7aa84b59004d4b9c4595d31e8b777b
José Atay <108359607+joseatay11@users.noreply.github.com>
2022-12-16 18:32:37 -0500

    Add files via upload

---
+++ Ejercicio-5/numbers.js
@@ -0,0 +1,11 @@
+let alturaCm = 180;
+let alturaM = 1.80;
+let pesoKg = 88.5;
+
+let alturaRedondeada = Math.ceil(alturaM);
+console.log(alturaRedondeada);
+let pesoRedondeado = Math.floor(pesoKg);
+console.log(pesoRedondeado);
+
+let esIgual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(esIgual);
