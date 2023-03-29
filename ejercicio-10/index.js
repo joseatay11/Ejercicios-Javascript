8bcd1356e88e3499581e3b51842a67848b8f9542
José Atay <108359607+joseatay11@users.noreply.github.com>
2023-03-29 15:49:17 -0500

    Add files via upload

---
+++ ejercicio-10/index.js
@@ -0,0 +1,5 @@
+import {suma, multiplica} from "./controller"
+import chalk from "chalk"
+
+//console.log(multiplica(suma(1,2), suma(6,7)))
+console.log(chalk.green(multiplica(suma(1, 2), suma(6,7))))
